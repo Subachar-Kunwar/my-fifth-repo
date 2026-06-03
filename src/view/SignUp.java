baebd899ca7d79e6ae8cab6737b96f114a5a40e6
DC-Samir <dcsamir584@gmail.com>
2026-06-03 15:12:31 +0545

    final for sprint one

--- src/view/SignUp.java
+++ src/view/SignUp.java
@@ -113,7 +113,7 @@ public class SignUp extends javax.swing.JFrame {
 
         txtEmail.addActionListener(this::txtEmailActionPerformed);
 
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/view/Logore.png"))); // NOI18N
+        // jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/view/Logore.png"))); // NOI18N
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
